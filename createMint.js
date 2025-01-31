ec8a28ab93fccce6881f45031664a6a20dddf0d0
Arif hussain <arif.hussoun898@gmail.com>
2025-02-01 04:35:44 +0500

    still  have errors to resolve

--- createMint.js
+++ createMint.js
@@ -1,7 +1,4 @@
 
-const mplumi =require("@metaplex-foundation/umi-bundle-defaults")
-const {createUmi}=mplumi;
-
 const web3 = require("@solana/web3.js");
 const {
   Keypair,
@@ -17,20 +14,19 @@ const {
   MINT_SIZE,
   createInitializeMint2Instruction,
   createInitializeInstruction,
-  createInitializeTransferFeeConfigInstruction, 
-
+  createInitializeTransferFeeConfigInstruction,
+  createInitializeMetadataPointerInstruction,
   ExtensionType,
-  
 } = Spltoken;
-const metaplex = require("@metaplex-foundation/mpl-token-metadata");
-const { MPL_TOKEN_METADATA_PROGRAM_ID, } = metaplex;
+/*const metaplex = require("@metaplex-foundation/mpl-token-metadata");
+const { MPL_TOKEN_METADATA_PROGRAM_ID } = metaplex;*/
 const keys = require("./Keypairs");
 const airdrop = require("./airdrop");
 const connection = new Connection(clusterApiUrl("devnet"));
 const { SendSol } = airdrop;
 const { payerSecretkey, mintSecretkey } = keys;
 
-const umi =createUmi(clusterApiUrl("devnet"))
+
 
 const tokenmetadata = {
   name: "Pak Rupees Token",
@@ -46,7 +42,79 @@ const mintkeypair = Keypair.fromSecretKey(mintSK);
 console.log("payer:", payer);
 console.log("mint keypair:", mintkeypair);
 
-const metadataProgramId= new PublicKey(MPL_TOKEN_METADATA_PROGRAM_ID)
+(async () => {
+  const extensions = [
+    ExtensionType.TransferFeeConfig,
+    ExtensionType.MetadataPointer,
+  ];
+  const mintlen = await Spltoken.getMintLen(extensions);
+
+  const fees = await connection.getMinimumBalanceForRentExemption(mintlen);
+
+  const mintAccountInstruction = SystemProgram.createAccount({
+    fromPubkey: payer.publicKey,
+    newAccountPubkey: mintkeypair.publicKey,
+    space: mintlen,
+    lamports: fees,
+    programId: TOKEN_2022_PROGRAM_ID,
+  });
+
+  console.log("mint account instructuion:", mintAccountInstruction);
+
+  const tranferFeeConfig = createInitializeTransferFeeConfigInstruction(
+    mintkeypair.publicKey,
+    payer.publicKey,
+    payer.publicKey,
+    500,
+    5000000,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  const metadataInstruction = createInitializeMetadataPointerInstruction(
+    mintkeypair.publicKey,
+    payer.publicKey,
+    mintkeypair.publicKey,
+    TOKEN_2022_PROGRAM_ID
+  );
+  const initializeMintInstruction = createInitializeMint2Instruction(
+    mintkeypair.publicKey,
+    6,
+    payer.publicKey,
+    payer.publicKey,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  const InitializeInstruction = createInitializeInstruction({
+    programId: TOKEN_2022_PROGRAM_ID,
+    mint: mintkeypair.publicKey,
+    metadata: mintkeypair.publicKey,
+    name: tokenmetadata.name,
+    symbol: tokenmetadata.symbol,
+    uri: tokenmetadata.uri,
+    mintAuthority: payer.publicKey,
+    updateAuthority: payer.publicKey,
+  });
+
+ 
+
+  const transactions = new web3.Transaction().add(
+    mintAccountInstruction,
+    tranferFeeConfig,
+    metadataInstruction, 
+    initializeMintInstruction, 
+    InitializeInstruction, 
+
+  );
+
+  const signature= await web3.sendAndConfirmTransaction(connection, transactions, [payer, mintkeypair], undefined
+  )
+
+  console.log(`🔗 Explorer URL: https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+  console.log("mint is : ", mintkeypair.publicKey);
+  console.log("payer is : ", payer.publicKey)
+})();
+
+/*const metadataProgramId= new PublicKey(MPL_TOKEN_METADATA_PROGRAM_ID)
   console.log( "mpl program id",MPL_TOKEN_METADATA_PROGRAM_ID)
   const metadataAccount = PublicKey.findProgramAddressSync([
     Buffer.from("metadata"),
@@ -57,37 +125,4 @@ const metadataProgramId= new PublicKey(MPL_TOKEN_METADATA_PROGRAM_ID)
   console.log("metdata account:", metadataAccount)
 
   const metaDataPDA=metadataAccount[0];
-  console.log("metadata PDA:", metaDataPDA)
-
-
-
-(async () => {
-  const extensions = [ExtensionType.TransferFeeConfig];
-const mintlen = await Spltoken.getMintLen(extensions + MINT_SIZE);
-
-const fees = await connection.getMinimumBalanceForRentExemption(mintlen);
-
-const mintAccountInstruction = SystemProgram.createAccount({
-  fromPubkey: payer.publicKey,
-  newAccountPubkey: mintkeypair.publicKey,
-  space: mintlen,
-  lamports: fees,
-  programId: TOKEN_2022_PROGRAM_ID,
-});
-console.log("mint account instructuion:", mintAccountInstruction)
-
-const initializeMintInstruction =createInitializeMint2Instruction(
-  mintkeypair.publicKey,
-  6, 
-  payer.publicKey, 
-  payer.publicKey, 
-  TOKEN_2022_PROGRAM_ID
-)
-
-
-const createMetadatainstruction =metaplex.createMasterEditionV3()
-
-const transactions = new  web3.Transaction().add()
-
-})();
-
+  console.log("metadata PDA:", metaDataPDA)*/
